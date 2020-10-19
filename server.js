8d73b0fd0dd425eb3ce0a90ba7bb22859ac47bdc
fesalbondaniel <fesalbondaniel@gmail.com>
2020-10-19 16:16:55 +0800

    test

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const path = require('path');
 const app = express();
 
 console.log(__dirname);
-app.use(express.static(__dirname + '/dist/APP'));
+//app.use(express.static(__dirname + '/dist/APP'));
+app.use(express.static(__dirname));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/APP/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(process.env.PORT || 8080);
